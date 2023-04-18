d1e661f698fcc5c6cf36248d32b067a7287feab4
Paulo <34099726+mrkmartin@users.noreply.github.com>
2023-04-18 18:25:18 +0800

    Create a functionality for calculator

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const screen = document.querySelector(".input");
+const buttons = document.querySelectorAll(".button");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
 
+// Add click event listeners to all buttons except for the equal and clear buttons
+buttons.forEach((button) => {
+  if (button !== equalButton && button !== clearButton) {
+    button.addEventListener("click", () => {
+      screen.value += button.value;
+    });
+  }
+});
+
+// Add click event listener to the equal button
+equalButton.addEventListener("click", () => {
+  if (screen.value === "") {
+    return;
+  }
+  screen.value = eval(screen.value);
+});
+
+// Add click event listener to the clear button
+clearButton.addEventListener("click", () => {
+  screen.value = "";
+});
